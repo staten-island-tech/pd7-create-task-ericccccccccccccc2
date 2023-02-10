fa28a5e69629e8b0db1534de0c957e95652c5e1c
ericccccccccccccc2 <113203325+ericccccccccccccc2@users.noreply.github.com>
2023-02-10 14:10:25 -0500

    asdfasd

--- poopy/main.js
+++ poopy/main.js
@@ -1,42 +1,194 @@
 let cards = [
-  { name: "s2", value: "2" },
-  { name: "s3", value: "3" },
-  { name: "s4", value: "4" },
-  { name: "s5", value: "5" },
-  { name: "s6", value: "6" },
-  { name: "s7", value: "7" },
-  { name: "s8", value: "8" },
-  { name: "s9", value: "9" },
-  { name: "s10", value: "10" },
-  { name: "sj", value: "10" },
-  { name: "sq", value: "10" },
-  { name: "sk", value: "10" },
-  { name: "sa", value: "11" },
-  { name: "h2", value: "2" },
-  { name: "h3", value: "3" },
-  { name: "h4", value: "4" },
-  { name: "h5", value: "5" },
-  { name: "h6", value: "6" },
-  { name: "h7", value: "7" },
-  { name: "h8", value: "8" },
-  { name: "h9", value: "9" },
-  { name: "h10", value: "10" },
-  { name: "hj", value: "10" },
-  { name: "hq", value: "10" },
-  { name: "hk", value: "10" },
-  { name: "ha", value: "11" },
-  { name: "c2", value: "2" },
-  { name: "c3", value: "3" },
-  { name: "c4", value: "4" },
-  { name: "c5", value: "5" },
-  { name: "c6", value: "6" },
-  { name: "c7", value: "7" },
-  { name: "c8", value: "8" },
-  { name: "c9", value: "9" },
-  { name: "c10", value: "10" },
-  { name: "cj", value: "10" },
-  { name: "cq", value: "10" },
-  { name: "ck", value: "10" },
+  {
+    name: "s2",
+    value: "2",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/p2.png",
+  },
+  {
+    name: "s3",
+    value: "3",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/p3.png",
+  },
+  {
+    name: "s4",
+    value: "4",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/p4.png",
+  },
+  {
+    name: "s5",
+    value: "5",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/p5.png",
+  },
+  {
+    name: "s6",
+    value: "6",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/p6.png",
+  },
+  {
+    name: "s7",
+    value: "7",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/p7.png",
+  },
+  {
+    name: "s8",
+    value: "8",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/p8.png",
+  },
+  {
+    name: "s9",
+    value: "9",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/p9.png",
+  },
+  {
+    name: "s10",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/p10.png",
+  },
+  {
+    name: "sj",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/pj.png",
+  },
+  {
+    name: "sq",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/pq.png",
+  },
+  {
+    name: "sk",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/pk.png",
+  },
+  {
+    name: "sa",
+    value: "11",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/pa.png",
+  },
+  {
+    name: "h2",
+    value: "2",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/s2.png",
+  },
+  {
+    name: "h3",
+    value: "3",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/s3.png",
+  },
+  {
+    name: "h4",
+    value: "4",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/s4.png",
+  },
+  {
+    name: "h5",
+    value: "5",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/s5.png",
+  },
+  {
+    name: "h6",
+    value: "6",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/s6.png",
+  },
+  {
+    name: "h7",
+    value: "7",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/s7.png",
+  },
+  {
+    name: "h8",
+    value: "8",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/s8.png",
+  },
+  {
+    name: "h9",
+    value: "9",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/s9.png",
+  },
+  {
+    name: "h10",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/s10.png",
+  },
+  {
+    name: "hj",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/sj.png",
+  },
+  {
+    name: "hq",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/sq.png",
+  },
+  {
+    name: "hk",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/sk.png",
+  },
+  {
+    name: "ha",
+    value: "11",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/sa.png",
+  },
+  {
+    name: "c2",
+    value: "2",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/k2.png",
+  },
+  {
+    name: "c3",
+    value: "3",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/k3.png",
+  },
+  {
+    name: "c4",
+    value: "4",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/k4.png",
+  },
+  {
+    name: "c5",
+    value: "5",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/k5.png",
+  },
+  {
+    name: "c6",
+    value: "6",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/k6.png",
+  },
+  {
+    name: "c7",
+    value: "7",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/k7.png",
+  },
+  {
+    name: "c8",
+    value: "8",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/k8.png",
+  },
+  {
+    name: "c9",
+    value: "9",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/k9.png",
+  },
+  {
+    name: "c10",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/k10.png",
+  },
+  {
+    name: "cj",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/kj.png",
+  },
+  {
+    name: "cq",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/kq.png",
+  },
+  {
+    name: "ck",
+    value: "10",
+    img: "https://www.improvemagic.com/wp-content/uploads/2020/11/kk.png",
+  },
   { name: "ca", value: "11" },
   { name: "d2", value: "2" },
   { name: "d3", value: "3" },
@@ -61,7 +213,7 @@ function dealUser(array) {
   let temp = (Math.random() * array.length) | 0;
   let x = array.splice(temp, 1)[0];
   hand.push(x.name);
-  playhand.insertAdjacentElement("afterbegin", `<p>amongus</p>`);
+  playhand.insertAdjacentHTML("afterbegin", `<p>amongus</p>`);
   return x;
 }
 
